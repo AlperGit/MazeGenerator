e9f9456ee7be735e152aa2204e129e8c10bdf278
AlperGit <alperdevelop@gmail.com>
2023-12-10 12:50:24 +0100

    Added Random numbers for initmaze function

--- Source-Files/Source.c
+++ Source-Files/Source.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "../Include-Files/Stack.h"
+#include "../Include-Files/Maze.h"
 
 int main()
 {
-    Cell cellA;
-    cellA.position.x = 0;
-    cellA.position.y = 0;
-
-    Cell cellB;
-    cellB.position.x = 5;
-    cellB.position.y = 5;
-
-    printf("Is Empty: %d\n", isempty());
-    push(cellB);
-    push(cellA);
-    printf("%f:%f \n", peek().position.x, peek().position.y);
-    printf("Is Full: %d\n", isfull());
+    //Set grid size
+    Vector2 gridSize;
+    gridSize.x = 3;
+    gridSize.y = 3;
+    //Create a random starting position for x and y
+    srand(0);
+    Vector2 startingPos;
+    startingPos.x = rand() % gridSize.x;
+    startingPos.y = rand() % gridSize.y;
+    //Create a random number cells to generate
+    int amountCellsToGenerate = rand() % (gridSize.x * gridSize.y);
     
+    //Initialize the maze array and assign memory
+    initMaze(gridSize, startingPos, amountCellsToGenerate);
+    //Generate maze with specified amount of cells
+    //Display Maze on the screen
 }
